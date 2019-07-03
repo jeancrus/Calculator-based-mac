cf98315a3790c60fa75492dca53459bb7b0c148e
Jean Crus <jean.carlos@pucpr.edu.br>
2019-07-03 00:37:05 -0300

    Using javascript to do operations and digits in the calculator

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,32 @@
 import React from 'react'
 import './Button.css'
 
+
 export default props => {
-    return <button className='button'>{props.label}</button>
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    return (
+        <button
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
+    )
+
 }
+
+// export default props => {
+//     return <button
+    // onClick={e => props.click && props.click(e.target.innerHTML)}
+    //  className={`
+//         button
+//         ${props.operation ? 'operation' : ''}
+//         ${props.double ? 'double' : ''}
+//         ${props.triple ? 'triple' : ''}
+//     `}>
+//         {props.label}
+//     </button>
+// }
